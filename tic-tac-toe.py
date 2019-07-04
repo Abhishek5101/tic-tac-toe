a9929f1f5fa61f2511e0703ce285c3d38d04d062
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-07-04 18:32:30 +0530

    removed unnecessary global kw use

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -6,6 +6,18 @@ either a 0 or a 1. When they do the respective player wins
 """
 
 co_ordinate = [7, 7, 7, 7, 7, 7, 7, 7, 7]
+
+
+def user_input_one():
+    position = int(input("Player 1: Enter position: "))
+    co_ordinate.insert(position, 0)
+
+
+def user_input_two():
+    position = int(input("Player 2: Enter position: "))
+    co_ordinate.insert(position, 1)
+
+
 co_ordinate_1 = [co_ordinate[0], co_ordinate[1], co_ordinate[2]]
 co_ordinate_2 = [co_ordinate[3], co_ordinate[4], co_ordinate[5]]
 co_ordinate_3 = [co_ordinate[6], co_ordinate[7], co_ordinate[8]]
@@ -15,18 +27,9 @@ co_ordinate_6 = [co_ordinate[2], co_ordinate[5], co_ordinate[8]]
 co_ordinate_7 = [co_ordinate[0], co_ordinate[4], co_ordinate[8]]
 co_ordinate_8 = [co_ordinate[2], co_ordinate[4], co_ordinate[6]]
 
+
 co_ordinate_to_check = [co_ordinate_1, co_ordinate_2, co_ordinate_3, co_ordinate_4,
                         co_ordinate_5, co_ordinate_6, co_ordinate_7, co_ordinate_8]
-
-
-def user_input_one():
-    position = int(input("Player 1: Enter position: "))
-    co_ordinate[position] = 0
-
-
-def user_input_two():
-    position = int(input("Player 2: Enter position: "))
-    co_ordinate[position] = 1
 
 
 def game_over():
@@ -40,11 +43,11 @@ def game_over():
 keepgoing = True
 while keepgoing:
     user_input_one()
-    print(co_ordinate, co_ordinate_to_check)
+    print(co_ordinate)
     if game_over():
         keepgoing = False
     user_input_two()
-    print(co_ordinate, co_ordinate_to_check)
+    print(co_ordinate)
     if game_over():
         keepgoing = False
 
